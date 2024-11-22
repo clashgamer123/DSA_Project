1c358904b717219cda7d9308896650bd2d0ae21d
clashgamer123 <164028744+clashgamer123@users.noreply.github.com>
2024-11-23 00:57:59 +0530

    Update plagiarism_checker.hpp

--- plagiarism_checker/phase2/plagiarism_checker.hpp
+++ plagiarism_checker/phase2/plagiarism_checker.hpp
@@ -1,13 +1,32 @@
 #include "structures.hpp"
 // -----------------------------------------------------------------------------
-
+#include <chrono>
+#include <set>
+#include <mutex>
+#include <thread>
 // You are free to add any STL includes above this comment, below the --line--.
 // DO NOT add "using namespace std;" or include any other files/libraries.
 // Also DO NOT add the include "bits/stdc++.h"
 
 // OPTIONAL: Add your helper functions and classes here
 
-class plagiarism_checker_t {
+struct submission_data_t
+{
+    std :: vector<int> tokens ;
+    int time_stamp ;
+    std :: shared_ptr<submission_t> addr ;
+    int no_matches ; 
+    bool isPlagged ;
+
+    public:
+    submission_data_t(void) ;
+    submission_data_t(std :: vector<int>& tokens, int& time_stamp, std :: shared_ptr<submission_t> addr) ;
+    ~submission_data_t(void) ;
+};
+
+
+class plagiarism_checker_t 
+{
     // You should NOT modify the public interface of this class.
 public:
     plagiarism_checker_t(void);
@@ -17,7 +36,12 @@ public:
     void add_submission(std::shared_ptr<submission_t> __submission);
 
 protected:
-    // TODO: Add members and function signatures here
-    
-    // End TODO
+    // vector of submissions
+    std :: vector<submission_data_t> DataBase ;
+    std::chrono::time_point<std::chrono::steady_clock> start_time;
+
+    // function to check plag
+    void plagiarism_check(int index) ;
+    bool pair_wise_plag(const std :: vector<int>& sub1, const std :: vector<int>& sub2, int& no_matches) ;
+    void plag_this_submission(std :: shared_ptr<submission_t>& addr) ;
 };
