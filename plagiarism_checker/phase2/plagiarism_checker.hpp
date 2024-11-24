26bbcb7d92cf498f6ab52697b3725c2d3cf23a93
clashgamer123 <164028744+clashgamer123@users.noreply.github.com>
2024-11-25 02:22:18 +0530

    Update plagiarism_checker.hpp

--- plagiarism_checker/phase2/plagiarism_checker.hpp
+++ plagiarism_checker/phase2/plagiarism_checker.hpp
@@ -7,6 +7,8 @@
 #include <mutex>
 #include <condition_variable>
 #include <queue>
+#include <unordered_map>
+
 // You are free to add any STL includes above this comment, below the --line--.
 // DO NOT add "using namespace std;" or include any other files/libraries.
 // Also DO NOT add the include "bits/stdc++.h"
@@ -45,10 +47,10 @@ protected:
 
     // function to check plag
     void plagiarism_check(int index) ;
-    bool pair_wise_plag(const std :: vector<int>& sub1, const std :: vector<int>& sub2, int& no_matches) ;
+    bool pair_wise_plag(const std :: unordered_map<long long, int>& map_curr, int curr, int prev, int& no_matches) ;
     void plag_this_submission(std :: shared_ptr<submission_t>& addr) ;
+    void hash_this_submission(int index, std :: unordered_map<long long, int>& hashed_map) ;
 
-private:
     // Number of original files
     int numOriginal ;
 
